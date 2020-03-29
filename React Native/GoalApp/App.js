60ed3f026248ec6fcd6f4eae44634643d8812ffc
unknown <hadarnetanel@outlook.com>
2020-03-29 10:01:01 +0300

    useState

--- React Native/GoalApp/App.js
+++ React Native/GoalApp/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enterGoal], [setEnterGoal] = useState=('')
   return (
     <View style={{padding:40}}>
-      <View style={{flexDirection:'row'}}>
-        <TextInput placeholder="Goal" style={{borderColor:'black', borderWidth:1}}/>
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+        <TextInput placeholder="Goal" style={{width:'80%', borderColor:'black', borderWidth:1, padding:10 }}/>
         <Button title="Add Goal"/>
       </View> 
       
