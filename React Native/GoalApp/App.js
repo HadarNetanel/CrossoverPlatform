67c882a1686514202e33485f156a905fe0812a37
unknown <hadarnetanel@outlook.com>
2020-03-29 10:47:32 +0300

    addGoalHandler

--- React Native/GoalApp/App.js
+++ React Native/GoalApp/App.js
@@ -1,45 +1,55 @@
-import React, {useState} from 'react';
+import React, { useState} from "react";
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 
-
 export default function App() {
-  const [enterGoal, setEnterGoal] = useState=('')
+const [enteredGoal, setEnteredGoal] = useState('');
 
-  const goalInputHandler = (enteredText) =>
-  setEnterGoal(enteredText);
+const goalInputHandler = (enteredText) => {
+  setEnteredGoal(enteredText);
+};
 
-  const addGoalHandler = () => {Console.log(enterGoal)};
+const addGoalHandler = () => {
+  console.log(enteredGoal);
+};
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputClass}>
-        <TextInput placeholder="Goal" style={styles.input} onChangeText={goalInputHandler} value={enterGoal}/>
-        <Button title="Add Goal" onPress={addGoalHandler}/>
+      <View style={styles.inputContainer}>
+        
+        <TextInput placeholder="Goal" 
+        style={styles.input} 
+        onChangeText={goalInputHandler} 
+        value={enteredGoal}/>
+        
+        <Button title="Add Goal" 
+        onPress={addGoalHandler}/>
       </View> 
       
       <View>  
      
       </View> 
      
-     </View>  );
+     </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen:{
-    padding:40
+  screen: {
+    padding: 40
 
   },
-  inputClass:{
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'center'
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   input:{
-    width:'80%',
-    borderColor:'black',
-    borderWidth:1, padding:10
-  }
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
 });
 
 
