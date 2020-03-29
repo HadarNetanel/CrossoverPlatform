314ecedea93e804ceec3175f19a8b02597fe8cd6
unknown <hadarnetanel@outlook.com>
2020-03-29 10:09:36 +0300

    onChangeText

--- React Native/GoalApp/App.js
+++ React Native/GoalApp/App.js
@@ -1,12 +1,17 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+
+
 export default function App() {
-  const [enterGoal], [setEnterGoal] = useState=('')
+  const [enterGoal, setEnterGoal] = useState=('')
+
+  const goalInputHandler = (enteredText) =>
+  setEnterGoal(enteredText)
   return (
-    <View style={{padding:40}}>
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-        <TextInput placeholder="Goal" style={{width:'80%', borderColor:'black', borderWidth:1, padding:10 }}/>
+    <View style={styles.screen}>
+      <View style={styles.inputClass}>
+        <TextInput placeholder="Goal" style={styles.input} onChangeText={goalInputHandler} value={enterGoal}/>
         <Button title="Add Goal"/>
       </View> 
       
@@ -18,6 +23,20 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  screen:{
+    padding:40
+
+  },
+  inputClass:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  input:{
+    width:'80%',
+    borderColor:'black',
+    borderWidth:1, padding:10
+  }
 });
 
 
